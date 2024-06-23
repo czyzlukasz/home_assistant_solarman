39ba8f589183ca5608dd0bf9b56078d1eb2de939
Łukasz Czyż <lczyz@softserveinc.com>
2024-06-23 11:17:31 +0200

    Inverter class can communicate using Modbus TCP or RDP

--- custom_components/solarman/solarman.py
+++ custom_components/solarman/solarman.py
@@ -7,6 +7,7 @@ from datetime import datetime
 from .parser import ParameterParser
 from .const import *
 from pysolarmanv5 import PySolarmanV5
+from pyModbusTCP.client import ModbusClient
 
 
 log = logging.getLogger(__name__)
@@ -14,13 +15,14 @@ log = logging.getLogger(__name__)
 QUERY_RETRY_ATTEMPTS = 2
 
 class Inverter:
-    def __init__(self, path, serial, host, port, mb_slaveid, lookup_file):
+    def __init__(self, path, serial, host, port, mb_slaveid, tcp, lookup_file):
         self._modbus = None
         self._serial = serial
         self.path = path
         self._host = host
         self._port = port
         self._mb_slaveid = mb_slaveid
+        self._tcp = tcp
         self._current_val = None
         self.status_connection = "Disconnected"
         self.status_lastUpdate = "N/A"
@@ -36,10 +38,13 @@ class Inverter:
         if self._modbus:
             return self._modbus
         log.info(f"Connecting to solarman data logger {self._host}:{self._port}")
-        self._modbus = PySolarmanV5(self._host, self._serial, port=self._port, mb_slave_id=self._mb_slaveid, logger=log, auto_reconnect=True, socket_timeout=15)
+        if self._tcp:
+            self._modbus = ModbusClient(host=self._host, port=self._port, timeout=15, auto_open=True, auto_close=True)
+        else:
+            self._modbus = PySolarmanV5(self._host, self._serial, port=self._port, mb_slave_id=self._mb_slaveid, logger=log, auto_reconnect=True, socket_timeout=15)
 
     def disconnect_from_server(self):
-        if self._modbus:
+        if self._modbus and not self._tcp:
             try:
                 log.info(f"Disconnecting from solarman data logger {self._host}:{self._port}")
                 self._modbus.disconnect()
@@ -47,14 +52,24 @@ class Inverter:
                 self._modbus = None
 
     def send_request(self, params, start, end, mb_fc):
+        if self._tcp:
+            self.send_request_tcp(params, start, end)
+        else:
+            self.send_request_rtu(params, start, end, mb_fc)
+
+    def send_request_rtu(self, params, start, end, mb_fc):
         length = end - start + 1
         match mb_fc:
             case 3:
-                response  = self._modbus.read_holding_registers(register_addr=start, quantity=length)
+                response = self._modbus.read_holding_registers(register_addr=start, quantity=length)
             case 4:
-                response  = self._modbus.read_input_registers(register_addr=start, quantity=length)
-        params.parse(response, start, length)        
+                response = self._modbus.read_input_registers(register_addr=start, quantity=length)
+        params.parse(response, start, length)
 
+    def send_request_tcp(self, params, start, end):
+        length = end - start + 1
+        response = self._modbus.read_holding_registers(start, length)
+        params.parse(response, start, length)
 
     @Throttle (MIN_TIME_BETWEEN_UPDATES)
     def update (self):
@@ -125,8 +140,11 @@ class Inverter:
     def service_write_holding_register(self, register, value):
         log.debug(f'Service Call: write_holding_register : [{register}], value : [{value}]')
         try:
-            self.connect_to_server()
-            self._modbus.write_holding_register(register, value)
+            if self._tcp:
+                self._modbus.write_single_register(register, value)
+            else:
+                self.connect_to_server()
+                self._modbus.write_holding_register(register, value)
         except Exception as e:
             log.warning(f"Service Call: write_holding_register : [{register}], value : [{value}] failed with exception [{type(e).__name__}: {e}]")
             self.disconnect_from_server()
@@ -135,8 +153,11 @@ class Inverter:
     def service_write_multiple_holding_registers(self, register, values):
         log.debug(f'Service Call: write_multiple_holding_registers: [{register}], values : [{values}]')
         try:
-            self.connect_to_server()
-            self._modbus.write_multiple_holding_registers(register, values)
+            if self._tcp:
+                self._modbus.write_multiple_registers(register, values)
+            else:
+                self.connect_to_server()
+                self._modbus.write_multiple_holding_registers(register, values)
         except Exception as e:
             log.warning(f"Service Call: write_multiple_holding_registers: [{register}], values : [{values}] failed with exception [{type(e).__name__}: {e}]")
             self.disconnect_from_server()
